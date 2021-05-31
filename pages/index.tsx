1416d67cb65c1615ae99441af01443f98ddacdfd
Plinio <plinio.duartes@hotmail.com>
2021-05-31 17:14:50 -0300

    a

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Teste Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Página de busca de Franquias
         </h1>
 
         <p className={styles.description}>
