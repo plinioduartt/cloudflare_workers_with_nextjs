9112688103f3654d90b23e599973d329f4f2964b
Plinio <plinio.duartes@hotmail.com>
2021-05-31 17:54:13 -0300

    fix: a

---
+++ pages/paulinia/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const paulinia: React.FC = () => {
+  return (
+    <h1>Unidade de Paulínia</h1>
+  );
+}
+
+export default paulinia;
